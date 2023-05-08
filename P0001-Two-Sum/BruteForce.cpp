0100bffc095efa697a24004870e7a7d6d8651178
Yi Zhen <103614898+eezhen@users.noreply.github.com>
2023-05-08 13:41:43 +0800

    Update BruteForce.cpp

--- P0001-Two-Sum/BruteForce.cpp
+++ P0001-Two-Sum/BruteForce.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v(2);
+        int len=nums.size();
+        for(int i=0;i<len-1;i++)
+            for(int j=i+1;j<len;j++)
+                if(target == nums[i]+nums[j]){
+                    v[0] = i;
+                    v[1] = j;
+                }
+        return v;
+    }
+};
