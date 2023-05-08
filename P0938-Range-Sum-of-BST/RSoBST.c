0bef75f5f7b93f44bb4228694c892df644a1bbf9
Yi Zhen <103614898+eezhen@users.noreply.github.com>
2023-05-08 13:44:47 +0800

    Update RSoBST.c

--- P0938-Range-Sum-of-BST/RSoBST.c
+++ P0938-Range-Sum-of-BST/RSoBST.c
@@ -1,1 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+void PrevOrder(struct TreeNode* p, int* sum, int low, int high) {
+    if (p == NULL) {
+	return;
+    }
+    if(p->val >= low && p->val <= high){
+	(*sum) += p->val;
+    }
+    PrevOrder(p->left,sum,low,high);
+    PrevOrder(p->right,sum,low,high);
+}
 
+int rangeSumBST(struct TreeNode* root, int low, int high){
+    int sum = 0;
+    PrevOrder(root,&sum,low,high);
+    return sum;
+}
